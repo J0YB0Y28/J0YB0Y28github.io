deb97f908b9feaf565f18743cf9c4c5dc7b2bcc5
Teddy Kana <tekab2@ulaval.ca>
2024-02-13 00:41:32 -0500

    mise à jour du site

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,12 @@
 		});
 
 })(jQuery);
+
+function toggle_text(id) {
+	var span = document.getElementById(id);
+	if(span.style.display == "none") {
+	  span.style.display = "inline";
+	} else {
+	  span.style.display = "none";
+	}
+  }
